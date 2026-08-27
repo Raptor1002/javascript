079c8106b89bcd6a362e5dee0581ad386f3e38f3
MohanRaj <155159156+Raptor1002@users.noreply.github.com>
2026-08-27 12:33:07 +0530

    Add files via upload

---
+++ JavaScript/practical_script.js
@@ -0,0 +1,16 @@
+//Create a variable for student name, age, and mark and print all three.
+let studentName = "Alice";
+let age = 20;
+let mark = 95;
+console.log(studentName, age, mark);
+
+//Ask the user's name using prompt() and display it using alert()
+let userName = prompt("whats is your name?");
+alert(userName);
+
+// Ask the user a question using confirm().
+let isReady =confirm("shall we go?");
+
+// Ask the user's name and display it on the webpage using document.writeln().
+let visitorName = prompt("Please enter your name:");
+document.writeln("Welcome to the page, " + visitorName);
